4b7ba645c67dd9b63cfb8778e0793716b3991f97
dxy2798 <dxy9900@gmail.com>
2019-04-19 17:01:35 +0800

    修改1

--- pdfToJpg.py
+++ pdfToJpg.py
@@ -1,6 +1,6 @@
 from wand.image import Image
 
-with Image(filename="k:/Scan1.pdf", resolution=300) as img:
+with Image(filename="d:/sys_bak/0010900115700808_099-1651807210035.pdf", resolution=300) as img:
     img.format = 'jpeg'
     img.save(filename='k:/Scan1.jpg')
 
